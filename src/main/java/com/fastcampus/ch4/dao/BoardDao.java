53291096e6bf45593da33a44c663ef89981af498
Min Woo Jeon <jeonminwoo2000@gmail.com>
2024-03-23 23:40:00 +0900

    comment

--- src/main/java/com/fastcampus/ch4/dao/BoardDao.java
+++ src/main/java/com/fastcampus/ch4/dao/BoardDao.java
@@ -18,4 +18,6 @@ public interface BoardDao {
 
     List<BoardDto> searchSelectPage(SearchCondition sc) throws Exception;
     int searchResultCnt(SearchCondition sc) throws Exception;
+
+    int updateCommentCnt(Integer bno, int cnt);
 }
